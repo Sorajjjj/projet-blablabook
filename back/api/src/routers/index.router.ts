a3821396a3e266bba2a24bad8711fc9cd45fa81b
Mia <amelia.decobert@gmail.com>
2026-01-13 14:27:40 +0100

    feature: create home router and update index router

--- back/api/src/routers/index.router.ts
+++ back/api/src/routers/index.router.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { healthCheck } from "../controllers/main.controller.js"
+import { healthCheck } from "../controllers/main.controller.js";
+// Import home router
+import homeRouter from "./home.router.js";
 
 export const router = Router();
 
 router.get("/health", healthCheck);
+
+// Use router to get home page
+router.use("/", homeRouter);

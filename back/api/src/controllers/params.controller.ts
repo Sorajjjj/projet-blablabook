271fc6b5a69800a0a956f2a27510c95a6ab91ac1
Mia <amelia.decobert@gmail.com>
2026-01-19 19:52:45 +0100

    update seed file; user create updated and userSettings create added; to manage settings page

---
+++ back/api/src/controllers/params.controller.ts
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import type { Request, Response, NextFunction } from "express";
+import { prisma } from "../config/prisma.js";
+import { paramsSchema } from "../schemas/uuidSchema.js";
+import { NotFoundError, UnauthorizedError } from "../lib/errors.js";
+
+export async function getParams(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    // Get the user id from the auth middleware
+    const userId = req.userId;
+
+    // If user id is not accepted, send 401 error
+    if (!userId) {
+      throw new UnauthorizedError("Non Autorisé");
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { userId },
+      select: {
+        username: true,
+        email: true,
+        isActive: true,
+        settings: {
+          select: {
+            theme: true,
+          },
+        },
+      },
+    });
+
+    // If user id does not exists, send 404 error
+    if (!user) {
+      throw new NotFoundError("Utilisateur inexistant");
+    }
+
+    return res.status(200).json({
+      username: user.username,
+      email: user.email,
+      isActive: user.isActive,
+      // If the user has settings and a theme, use it;
+      // Otherwise, use "light" as the default theme
+      theme: user.settings?.theme ?? "light",
+    });
+  } catch (error) {
+    next(error);
+  }
+}

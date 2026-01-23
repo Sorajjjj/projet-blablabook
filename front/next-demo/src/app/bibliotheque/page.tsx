2e7a83d5cd318001e2d494d02dc63d06655db190
student <mat13113@gmail.com>
2026-01-23 12:04:28 +0100

    feat: update sidebar navigation UI and hover effects

--- front/next-demo/src/app/bibliotheque/page.tsx
+++ front/next-demo/src/app/bibliotheque/page.tsx
@@ -6,42 +6,45 @@ import Header from "@/components/blablabook/Header";
 import Styles from "./library.module.css";
 import { OrangeOutlineButton, SimpleButton } from "@/components/blablabook/buttons";
 import BookCardLibrary from "@/components/blablabook/book-card-library";
+import { CheckCheck, BookMarked, BookHeart, BookCopy } from 'lucide-react';
 
 export default function LibraryPage() {
-	
+
 	// stocke le tableau de livres reçus de l'API (initialement vide [])
 	const [library, setLibrary] = useState<any[]>([]);
 	// affiche un message d'attente pendant que l'API répond (initialement vrai)
 	const [loading, setLoading] = useState(true);
 
-	
-const fetchLibrary = async () => {
-    try {
-        
-        const response = await fetch("http://localhost:4000/api/libraries", {
-            method: "GET",
-            credentials: "include", 
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        const result = await response.json();
-        
-        // On vérifie que result.data contient bien nos livres
-        setLibrary(result.data || []);
-    } catch (error) {
-        console.error("Erreur lors du chargement de la bibliothèque:", error);
-    } finally {
-        setLoading(false);
-    }
-};
+	const [activeTab, setActiveTab] = useState("all");
 
-	
+
+	const fetchLibrary = async () => {
+		try {
+
+			const response = await fetch("http://localhost:4000/api/libraries", {
+				method: "GET",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				}
+			});
+			const result = await response.json();
+
+			// On vérifie que result.data contient bien nos livres
+			setLibrary(result.data || []);
+		} catch (error) {
+			console.error("Erreur lors du chargement de la bibliothèque:", error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+
 	useEffect(() => {
 		fetchLibrary();
 	}, []);
 
-	
+
 	const handleDeleteBook = async (bookId: string) => {
 		//TODO MODIFIER et rajouter un composant
 		if (!confirm("Voulez-vous vraiment retirer ce livre ?")) return;
@@ -72,20 +75,44 @@ const fetchLibrary = async () => {
 					<h2 className={Styles.sidebarTitle}>Navigation</h2>
 					<ul className={Styles["sidebar-menu-ul"]}>
 						<li>
-							<OrangeOutlineButton className="py-5 w-full">
-								📕 Toute ma bibliothèque
-							</OrangeOutlineButton>
+							<SimpleButton
+								onClick={() => setActiveTab("all")}
+								className={`py-5 w-full ${activeTab === "all" ? Styles["active"] : ""}`}
+							>
+								<BookCopy /> Toute ma bibliothèque
+							</SimpleButton>
 						</li>
-						<li><SimpleButton className="pl-0 py-5">🤍 Favoris</SimpleButton></li>
-						<li><SimpleButton className="pl-1 py-5">📖 En cours</SimpleButton></li>
-						<li><SimpleButton className="pl-0 py-5">✔️ Lus</SimpleButton></li>
+						<li>
+							<SimpleButton
+								onClick={() => setActiveTab("todo")}
+								className={`pl-0 py-5 ${activeTab === "todo" ? Styles["active"] : ""}`}
+							>
+								<BookHeart /> A lire
+							</SimpleButton>
+						</li>
+						<li>
+							<SimpleButton
+								onClick={() => setActiveTab("doing")}
+								className={`pl-1 py-5 ${activeTab === "doing" ? Styles["active"] : ""}`}
+							>
+								<BookMarked /> En cours
+							</SimpleButton>
+						</li>
+						<li>
+							<SimpleButton
+								onClick={() => setActiveTab("done")}
+								className={`pl-0 py-5 ${activeTab === "done" ? Styles["active"] : ""}`}
+							>
+								<CheckCheck /> Lus
+							</SimpleButton>
+						</li>
 					</ul>
 				</aside>
 
 				{/* SECTION PRINCIPALE (CONTENU) */}
 				<section className={Styles.content}>
 					<p className={Styles.breadcrumb}>Bibliothèque / Tous les livres</p>
-					
+
 					<div className="flex justify-between items-center mb-10">
 						<h1 className={Styles.title}>MA BIBLIOTHÈQUE</h1>
 						{/* FILTRES (Affichage uniquement pour l'instant) */}
@@ -100,23 +127,23 @@ const fetchLibrary = async () => {
 					<div className="flex flex-col gap-6">
 						{loading ? (
 							<p className="text-gray-500 italic text-center">Chargement de vos livres...</p>
-						) : 
-						
-						library.length > 0 ? (
-							
-							library.map((item) => (
-								<BookCardLibrary 
-									key={item.bookId}
-									book={item.book} 
-									status={item.status} 
-									addedAt={item.createdAt} 
-									
-									onDelete={() => handleDeleteBook(item.bookId)} 
-								/>
-							))
-						) : (
-							<p className="text-gray-500 italic text-center">Votre bibliothèque est vide.</p>
-						)}
+						) :
+
+							library.length > 0 ? (
+
+								library.map((item) => (
+									<BookCardLibrary
+										key={item.bookId}
+										book={item.book}
+										status={item.status}
+										addedAt={item.createdAt}
+
+										onDelete={() => handleDeleteBook(item.bookId)}
+									/>
+								))
+							) : (
+								<p className="text-gray-500 italic text-center">Votre bibliothèque est vide.</p>
+							)}
 					</div>
 				</section>
 			</main>

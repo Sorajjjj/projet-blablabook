6b6bb217cadc4419e0ffd6e1292d3e1d1267b245
Ines Samel <ines.samel@gmx.com>
2026-01-28 15:05:06 +0100

    privacy page created + fixed the hydration bug + fixed the detection when the cookie is cleared in the browser so the avatar does not appear instead of the login and sign in buttons

--- back/prisma/migrations/20260128132934_init/migration.sql
+++ back/prisma/migrations/20260128132934_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "user" (
     "username" VARCHAR(50) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password_hash" VARCHAR(255) NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -27,6 +28,7 @@ CREATE TABLE "book" (
     "release_date" DATE,
     "isbn" VARCHAR(13),
     "summary" TEXT,
+    "image_url" VARCHAR(255),
     "author_id" UUID NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -56,11 +58,20 @@ CREATE TABLE "book_genre" (
 CREATE TABLE "user_library" (
     "user_id" UUID NOT NULL,
     "book_id" UUID NOT NULL,
-    "status" VARCHAR(20) NOT NULL DEFAULT 'want_to_read',
+    "status" VARCHAR(20) NOT NULL DEFAULT 'A lire',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "user_library_pkey" PRIMARY KEY ("user_id","book_id")
+);
+
+-- CreateTable
+CREATE TABLE "UserSettings" (
+    "settings_id" UUID NOT NULL,
+    "theme" VARCHAR(20) NOT NULL DEFAULT 'light',
+    "user_id" UUID NOT NULL,
+
+    CONSTRAINT "UserSettings_pkey" PRIMARY KEY ("settings_id")
 );
 
 -- CreateIndex
@@ -74,6 +85,9 @@ CREATE UNIQUE INDEX "book_isbn_key" ON "book"("isbn");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "genre_name_key" ON "genre"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSettings_user_id_key" ON "UserSettings"("user_id");
 
 -- AddForeignKey
 ALTER TABLE "book" ADD CONSTRAINT "book_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "author"("author_id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -89,3 +103,6 @@ ALTER TABLE "user_library" ADD CONSTRAINT "user_library_user_id_fkey" FOREIGN KE
 
 -- AddForeignKey
 ALTER TABLE "user_library" ADD CONSTRAINT "user_library_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "book"("book_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserSettings" ADD CONSTRAINT "UserSettings_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
